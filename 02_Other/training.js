e9ffa2304c6f0363c02a7566295051c3468594d2
Yurii <juravyn@gmail.com>
2021-04-29 21:51:59 +0300

    29.04.2021

--- 02_Other/training.js
+++ 02_Other/training.js
@@ -1,3 +1,18 @@
 "use strict"
+const alex = {
+    name: "Alex",
+    age: 18,
+    adress: {
+        country: "USA",
+        street: "Nova"
+    }
+}
 
+const pete = {}
 
+function copy (obj) {
+    for (let key in obj) {
+        console.log(key)
+    }
+}
+copy(alex)
