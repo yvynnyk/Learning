9f4bf799024b21663521063267b2744a946067be
Yurii <juravyn@gmail.com>
2021-05-02 12:42:56 +0300

    02.05.2021

--- 01_CodeWars/01_js.js
+++ 01_CodeWars/01_js.js
@@ -91,7 +91,7 @@ var countBits = function(n) {
 //findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
 
 function findUniq(arr) {
-    return arr.sort()[0] == arr.sort()[1] ? arr.sort()[arr.length - 1] : arr.sort()[0]
+    return arr.sort()[0] == arr.sort()[1] ? arr.sort()[arr.length - 1] : arr.sort()[0];
 }
   
 //[2, 4, 0, 100, 4, 11, 2602, 36]
@@ -101,7 +101,7 @@ function findUniq(arr) {
 //Should return: 160 (the only even number)
 
 function findOutlier(integers){
-    return integers.filter(x => x % 2 == 0).length == 1 ? integers.filter(x => x % 2 == 0)[0] : integers.filter(x => x % 2 != 0)[0]
+    return integers.filter(x => x % 2 == 0).length == 1 ? integers.filter(x => x % 2 == 0)[0] : integers.filter(x => x % 2 != 0)[0];
 }
 
 
@@ -113,12 +113,12 @@ function findOutlier(integers){
 //numbers.even();    // must return [2, 4]
 //numbers.odd();     // must return [1, 3, 5]
 
-Array.prototype.square  = function () { return this.map(function(n) { return n*n; }); }
-Array.prototype.cube    = function () { return this.map(function(n) { return n*n*n; }); }
-Array.prototype.average = function () { return this.sum() / this.length; }
-Array.prototype.sum     = function () { return this.reduce(function(a, b) { return a + b; }, 0); }
-Array.prototype.even    = function () { return this.filter(function(item) { return 0 == item % 2; }); }
-Array.prototype.odd     = function () { return this.filter(function(item) { return 0 != item % 2; }); }
+Array.prototype.square  = function () { return this.map(function(n) { return n*n; }); };
+Array.prototype.cube    = function () { return this.map(function(n) { return n*n*n; }); };
+Array.prototype.average = function () { return this.sum() / this.length; };
+Array.prototype.sum     = function () { return this.reduce(function(a, b) { return a + b; }, 0); };
+Array.prototype.even    = function () { return this.filter(function(item) { return 0 == item % 2; }); };
+Array.prototype.odd     = function () { return this.filter(function(item) { return 0 != item % 2; }); };
 
 //Object.assign(Array.prototype, {methods})
 
@@ -130,11 +130,11 @@ var uniqueInOrder=function(iterable){
     let res = [];
     for (let i = 0; i < iterable.length; i++) {
       if (iterable[i] != iterable[i+1]) {
-        res.push(iterable[i])
+        res.push(iterable[i]);
       }
     }
     return res;
-}
+};
 //prime
 function isPrime(num) {
     if (num <= 1) {
@@ -156,10 +156,10 @@ function isPrime(num) {
 
 //The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 function count (string) {  
-  let obj = {}
+  let obj = {};
   let arr = [...new Set (string)].forEach(char => {
     obj[char] = string.split("").filter(x => x == char).length;
-  })
+  });
   return obj;
 }
 
@@ -171,19 +171,19 @@ function solution(string) {
 }
 
 //Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
-function digital_root(n) {
-    let N = String(n).split("").reduce((a, b) => +a + +b, 0)
-    if ( N < 10) {return N}
-    return digital_root(N)
+function digitalRoot(n) {
+    let N = String(n).split("").reduce((a, b) => +a + +b, 0);
+    if ( N < 10) {return N;}
+    return digitalRoot(N);
   }
 
 //It should remove all values from list a, which are present in list b keeping their order.
 function arrayDiff(a, b) {
-    let arr = [...a]
+    let arr = [...a];
      b.forEach(d => {
        arr = arr.filter(x => x != d);
-     })
-     return arr
+     });
+     return arr;
 }
 
 arrayDiff([1,2,3], [1,2]);
@@ -196,32 +196,32 @@ function duplicateCount(text){
     let str = text.toLowerCase();
     for (let i = 0; i < text.length; i++) {
       if (str.length - str.split(text[i]).join("").length >= 2 ) {
-        str = str.split(text[i]).join("")
-        counter++
-        console.log(str , counter , i)
+        str = str.split(text[i]).join("");
+        counter++;
+        console.log(str , counter , i);
       }
     }
-    return counter
+    return counter;
   }
 
-  console.log(duplicateCount("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"))
+  console.log(duplicateCount("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"));
 
   //
 
 function iqTest(numbers){
     const l = numbers.split(" ").filter(x => +x % 2 == 0).length;
-    const a = numbers.split(" ").indexOf(numbers.split(" ").filter(x => +x % 2 == 0)[0])
-    const b = numbers.split(" ").indexOf(numbers.split(" ").filter(x => +x % 2 != 0)[0])
+    const a = numbers.split(" ").indexOf(numbers.split(" ").filter(x => +x % 2 == 0)[0]);
+    const b = numbers.split(" ").indexOf(numbers.split(" ").filter(x => +x % 2 != 0)[0]);
     
-    return l == 1 ? a + 1 : b + 1
+    return l == 1 ? a + 1 : b + 1;
 }
 
 
 String.prototype.camelCase=function(){
-    return this.length != 0 ? this.trim().split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join("") : ""
-}
+    return this.length != 0 ? this.trim().split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join("") : "";
+};
 
-  " camel case word".camelCase()
+  " camel case word".camelCase();
 
 
   // 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153, 1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
@@ -231,13 +231,13 @@ String.prototype.camelCase=function(){
 
 //solution('abcdef') // should return ['ab', 'cd', 'ef']
 
-let str = "abcdef"
+let str = "abcdef";
 
 function solution (str) {
     let arr = [];
     if (str.length % 2 == 0) {
         for (let i = 0; i < str.length; i += 2){
-            arr.push(str.slice(i, i + 2))
+            arr.push(str.slice(i, i + 2));
         }
     }
 }
@@ -259,8 +259,7 @@ function anagrams(word, words) {
     return arr;
     */
     
-    return words.filter(x => [...x].sort().join("") == [...word].sort().join(""))
-      
+    return words.filter(x => [...x].sort().join("") == [...word].sort().join(""));
   }
 
 //var list1 = [
@@ -270,6 +269,109 @@ function anagrams(word, words) {
 function addUsername(list) {
     list.map(obj => {
          obj.username = (obj.firstName).toLowerCase() + (obj.lastName)[0].toLowerCase() + ((new Date ()).getFullYear() - obj.age); 
-     })
+     });
    return list;
  }
+
+
+//////////
+
+function isMerge(s, part1, part2) {
+  if ([...s].sort().join("") == [...part1,...part2].sort().join("")){
+    let id1 = 0;
+    let id2 = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] == part1[id1]) {
+        console.log(s[i], part1[id1]);
+        id1 += 1;
+      }
+      else if (s[i] == part2[id2]) {
+        console.log(s[i], part2[id2]);
+        id2 += 1;
+      }
+      else {
+        return false;
+      }
+    }
+    
+  } else {
+    return false;
+  }
+  return true;
+}
+
+console.log(isMerge('codewars', 'code', 'wasr'));
+
+//////
+function strToHash(str){
+  const obj = {};
+  if (str.length == 0) {return obj;}
+  str.split(", ").map(x => x.split("=")).forEach(value => {
+    obj[value[0]] = +value[1];
+  });
+  return obj;
+}
+
+console.log(strToHash(""));
+
+/////
+function alphabetPosition(text) {
+  const a = "abcdefghijklmnopqrstuvwxyz";
+  const str = text.toLowerCase();
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (a.indexOf(str[i]) == -1) {continue;} 
+      arr.push(a.indexOf(str[i]) + 1);
+    }
+  return arr.join(" ");
+}
+
+alphabetPosition("abcd");
+
+////
+function findMissingLetter(array) {
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].codePointAt(0) - array[i - 1].codePointAt(0) > 1) {
+      return String.fromCodePoint(array[i].codePointAt(0) - 1);
+    }
+  }
+  return ' ';
+}
+
+
+const arr = [1,2,3,5,1,4,3];
+
+////
+function findOdd(A) {
+  const arr = [...new Set (A)].sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (A.filter( x => x == arr[i]).length % 2 != 0) {return arr[i];}
+  }
+  
+  return 0;
+}
+
+console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
+
+//////
+
+function sortArray(array) {
+  const index = array.map((x, i) => x % 2 != 0 ? i : -1).sort((a, b) => a - b);
+  const odd = array.filter((x, i) => x % 2 != 0).sort((a, b) => a - b) ;
+  console.log(odd);
+  console.log(index);
+  /*odd.forEach(number => {
+    
+  })
+  index.sort((a, b) => a - b)
+  console.log(index)
+  index.forEach((value, i) => {
+    array[value] = odd[i]
+  })
+  */
+  //return array
+}
+
+sortArray([1, 1, 5, 11, 2, 11, 111, 0]);
+
